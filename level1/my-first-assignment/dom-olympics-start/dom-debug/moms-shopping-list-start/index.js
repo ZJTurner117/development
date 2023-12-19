f9756c3e27fbfd9afec865ac9f022870db53a582
Zachariah Turner <turnerjzachariah@gmail.com>
2023-12-18 17:37:09 -0800

    moms-shopping-list

---
+++ level1/my-first-assignment/dom-olympics-start/dom-debug/moms-shopping-list-start/index.js
@@ -0,0 +1,28 @@
+var input = document.getElementById("add-todo");
+console.log('input:', input)
+    input.addEventListener("submit", function(event){
+    event.preventDefault()
+let addedItem = document.getElementById("input").value
+    input.title.value = ""  
+
+let newItem = document.createElement('li')
+    document.getElementById('list').append(newItem)
+    newItem.textContent = addedItem
+    //newItem.textContent = addedItem   
+
+let newEditButton = document.createElement('button') 
+    newEditButton.textContent = 'edit' 
+    //newEditButton.style = "width: 37.5px; height: 21px; margin: auto"     
+    document.getElementById('list').append(newEditButton)
+    newEditButton.setAttribute("id","myId")
+let newDeleteButton = document.createElement('button') 
+    newDeleteButton.textContent = 'X'  
+    //newDeleteButton.style = "width: 37.5px; height: 21px; float: right; margin: auto"                    
+    document.getElementById('list').append(newDeleteButton) 
+    newDeleteButton.setAttribute("id","myId")
+    newDeleteButton.addEventListener("click", () => {
+        newItem.remove()
+        newEditButton.remove()
+        newDeleteButton.remove()
+    })
+});
