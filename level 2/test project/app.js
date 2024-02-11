ed36c188c9196f78f1b6fe3be557a3ac80efa33f
ZJTurner117 <153025651+ZJTurner117@users.noreply.github.com>
2024-02-10 16:00:04 -0800

    Add files via upload

---
+++ level 2/test project/app.js
@@ -0,0 +1,8 @@
+const arr = [4, 2, 7, 1, 9, 5];
+arr.sort((a, b) => a - b)
+console.log(arr)
+
+
+let array = ['hello', 'world', 'javascript'];
+let upperCaseArray = array.map(item => item.toUpperCase());
+console.log(upperCaseArray);
